d94df7c5f65027ce7552a338e4032ed7517dca33
Bogdan Popescu <bogdan.p@mondly.com>
2024-08-20 14:52:53 +0300

    Extracted private helper handleLoadResponse

--- netfox_ios_demo/TextViewController.swift
+++ netfox_ios_demo/TextViewController.swift
@@ -25,27 +25,31 @@ final class TextViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
-    @IBAction private func tappedLoad(_ sender: Any) {
+	
+	@IBAction private func tappedLoad(_ sender: Any) {
         dataTask?.cancel()
 
         guard let url = URL(string: "https://api.chucknorris.io/jokes/random") else { return }
         let request = URLRequest(url: url)
         dataTask = session.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                self.handleCompletion(error: error.localizedDescription, data: data)
-            } else {
-                guard let data = data else { self.handleCompletion(error: "Invalid data", data: nil); return }
-                guard let response = response as? HTTPURLResponse else { self.handleCompletion(error: "Invalid response", data: data); return }
-                guard response.statusCode >= 200 && response.statusCode < 300 else { self.handleCompletion(error: "Invalid response code", data: data); return }
-                
-                self.handleCompletion(error: error?.localizedDescription, data: data)
-            }
+			self.handleLoadResponse(error, data, response)
         }
         
         dataTask?.resume()
     }
-    
+
+	private func handleLoadResponse(_ error: Error?, _ data: Data?, _ response: URLResponse?) {
+		if let error = error {
+			self.handleCompletion(error: error.localizedDescription, data: data)
+		} else {
+			guard let data = data else { self.handleCompletion(error: "Invalid data", data: nil); return }
+			guard let response = response as? HTTPURLResponse else { self.handleCompletion(error: "Invalid response", data: data); return }
+			guard response.statusCode >= 200 && response.statusCode < 300 else { self.handleCompletion(error: "Invalid response code", data: data); return }
+
+			self.handleCompletion(error: error?.localizedDescription, data: data)
+		}
+	}
+
     private func handleCompletion(error: String?, data: Data?) {
         DispatchQueue.main.async {
             

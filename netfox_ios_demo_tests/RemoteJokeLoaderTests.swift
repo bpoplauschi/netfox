f007a3de41a929a022a080a30a81dda4d4e62be4
Bogdan Popescu <bogdan.p@mondly.com>
2024-08-20 15:36:39 +0300

    Added loadNewJoke success test

---
+++ netfox_ios_demo_tests/RemoteJokeLoaderTests.swift
@@ -0,0 +1,33 @@
+//
+//  RemoteJokeLoaderTests.swift
+//  netfox_ios_demo_tests
+//
+//  Created by Bogdan on 20.08.2024.
+//  Copyright © 2024 kasketis. All rights reserved.
+//
+
+@testable import netfox_ios_demo
+import XCTest
+
+final class RemoteJokeLoaderTests: XCTestCase {
+	func test_loadNewJoke_onSuccess() {
+		let sut = makeSUT()
+		let exp = expectation(description: "Wait for completion")
+
+		sut.loadNewJoke(completion: { result in
+			guard let result else {
+				XCTFail("Missing result")
+				return
+			}
+			XCTAssertFalse(result.isEmpty)
+			exp.fulfill()
+		})
+
+		wait(for: [exp], timeout: 1.0)
+	}
+
+	private func makeSUT() -> RemoteJokeLoader {
+		let session = URLSession(configuration: .ephemeral)
+		return RemoteJokeLoader(session: session)
+	}
+}

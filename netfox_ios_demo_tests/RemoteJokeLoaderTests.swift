16130ff123239036b1190f7da7ac6b8d156e24a5
Bogdan Popescu <bogdan.p@mondly.com>
2024-08-20 16:10:39 +0300

    Added detailed tests for RemoteJokeLoader

--- netfox_ios_demo_tests/RemoteJokeLoaderTests.swift
+++ netfox_ios_demo_tests/RemoteJokeLoaderTests.swift
@@ -10,37 +10,117 @@
 import XCTest
 
 final class RemoteJokeLoaderTests: XCTestCase {
-	func test_loadNewJoke_onSuccess() {
-		let sut = makeSUT()
-		let exp = expectation(description: "Wait for completion")
+	override func setUp() {
+		super.setUp()
 
-		sut.loadNewJoke(completion: { result in
-			guard let result else {
-				XCTFail("Missing result")
-				return
-			}
-			XCTAssertFalse(result.isEmpty)
-			exp.fulfill()
-		})
-
-		wait(for: [exp], timeout: 1.0)
+		URLProtocolStub.removeStub()
 	}
 
-	func test_loadNewJoke_onCancel() {
+	override func tearDown() {
+		super.tearDown()
+
+		URLProtocolStub.removeStub()
+	}
+
+	func test_loadNewJoke_onAllEmptyValues() {
 		let sut = makeSUT()
-		let exp = expectation(description: "Wait for completion")
+
+		let result = loadNewJokeResultFor((data: nil, response: nil, error: nil), sut: sut)
+
+		XCTAssertNil(result)
+	}
+
+	func test_loadNewJoke_onError() {
+		let sut = makeSUT()
+
+		let result = loadNewJokeResultFor((data: nil, response: nil, error: NSError(domain: "", code: 0)), sut: sut)
+
+		XCTAssertNil(result)
+	}
+
+	func test_loadNewJoke_onHTTPSuccessWithoutData() {
+		let sut = makeSUT()
+
+		let result = loadNewJokeResultFor((data: nil, response: httpResponse(statusCode: 200), error: nil), sut: sut)
+
+		XCTAssertNil(result)
+	}
+
+	func test_loadNewJoke_onInvalidResponse() {
+		let sut = makeSUT()
+
+		let result = loadNewJokeResultFor((data: "any joke".data(using: .utf8), response: URLResponse(), error: nil), sut: sut)
+
+		XCTAssertNil(result)
+	}
+
+	func test_loadNewJoke_onHTTPNotSuccessCode() {
+		[0, 199, 300, 400, 401, 500, 505].forEach { statusCode in
+			let sut = makeSUT()
+			let jsonString = "{\"value\": \"any joke\"}"
+
+			let result = loadNewJokeResultFor((data: jsonString.data(using: .utf8), response: httpResponse(statusCode: statusCode), error: nil), sut: sut)
+
+			XCTAssertNil(result)
+		}
+	}
+
+	func test_loadNewJoke_onHTTPSuccessButNotJSON() {
+		let sut = makeSUT()
+
+		let result = loadNewJokeResultFor((data: "any joke".data(using: .utf8), response: httpResponse(statusCode: 200), error: nil), sut: sut)
+
+		XCTAssertNil(result)
+	}
+
+	func test_loadNewJoke_onHTTPSuccessWithJSONButMissingValueKey() {
+		let sut = makeSUT()
+		let jsonString = "{\"other key\": \"any joke\"}"
+
+		let result = loadNewJokeResultFor((data: jsonString.data(using: .utf8), response: httpResponse(statusCode: 200), error: nil), sut: sut)
+
+		XCTAssertNil(result)
+	}
+
+	func test_loadNewJoke_onSuccess() {
+		let sut = makeSUT()
+		let jsonString = "{\"value\": \"any joke\"}"
+
+		let result = loadNewJokeResultFor((data: jsonString.data(using: .utf8), response: httpResponse(statusCode: 200), error: nil), sut: sut)
+
+		XCTAssertEqual(result, "any joke")
+	}
+
+	private func loadNewJokeResultFor(
+		_ values: (data: Data?, response: URLResponse?, error: Error?)?,
+		sut: RemoteJokeLoader
+	) -> String? {
+		values.map { URLProtocolStub.stub(data: $0, response: $1, error: $2) }
+
+		let exp = expectation(description: "Wait for request")
+
+		var receivedResult: String?
 
 		sut.loadNewJoke(completion: { result in
-			XCTAssertNil(result)
+			receivedResult = result
 			exp.fulfill()
 		})
-		sut.cancelLoad()
 
 		wait(for: [exp], timeout: 1.0)
+
+		return receivedResult
 	}
 
 	private func makeSUT() -> RemoteJokeLoader {
-		let session = URLSession(configuration: .ephemeral)
+		let configuration = URLSessionConfiguration.ephemeral
+		configuration.protocolClasses = [URLProtocolStub.self]
+		let session = URLSession(configuration: configuration)
 		return RemoteJokeLoader(session: session)
 	}
+}
+
+func anyURL() -> URL { URL(string: "http://any-url.com")! }
+
+func httpResponse(statusCode: Int) -> HTTPURLResponse {
+	HTTPURLResponse(url: anyURL(), statusCode: statusCode, httpVersion: "HTTP/1.1", headerFields: [:])!
 }
